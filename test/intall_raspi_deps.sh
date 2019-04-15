4b285bd9d46f1d5e1c66133ab7f2a3a8af7efd84
Julian Gaal <juliangaal@users.noreply.github.com>
2019-04-15 21:10:33 +0200

    typo

--- test/intall_raspi_deps.sh
+++ test/intall_raspi_deps.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sudo apt-get update && sudp apt-get upgrade
+sudo apt-get update && sudo apt-get upgrade
 sudo apt-get install git python3-pip tmux vim -y
 pip install smbus
 git clone https://github.com/juliangaal/mpu6050.git
