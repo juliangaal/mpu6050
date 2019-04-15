e99e3525275af06faf7dcac0bd068fcc96876192
Julian Gaal <juliangaal@protonmail.com>
2019-04-15 23:47:55 +0200

    remove test dir -> kept in branch python-test

--- test/intall_raspi_deps.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-sudo apt-get update && sudo apt-get upgrade
-sudo apt-get install git python3-pip tmux vim -y
-pip install smbus
-git clone https://github.com/juliangaal/mpu6050.git
