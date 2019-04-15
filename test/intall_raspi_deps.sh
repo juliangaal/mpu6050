44584d8cacc09fa21ba1f9018628f5c9a51ebd70
Julian Gaal <juliangaal@users.noreply.github.com>
2019-04-15 21:10:12 +0200

    installation script for testing

--- test/intall_raspi_deps.sh
+++ test/intall_raspi_deps.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-sudo apt-get install git
+sudo apt-get update && sudp apt-get upgrade
+sudo apt-get install git python3-pip tmux vim -y
+pip install smbus
 git clone https://github.com/juliangaal/mpu6050.git
