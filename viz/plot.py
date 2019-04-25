263c1d99fe692281cd8d191ddca67d92be9771c7
Julian Gaal <gjulian@uos.de>
2019-04-25 12:48:52 +0200

    subplot for pitch and roll

--- viz/plot.py
+++ viz/plot.py
@@ -12,54 +12,47 @@ def plot(filename: str, filename_2: str, num_data_points: int):
     n = num_data_points
 
     f, axarr = plt.subplots(2, sharex=True)
-    f.suptitle('Roll (top) and Pitch (bottom)')
-    axarr[0].plot(x, y)
-    axarr[1].scatter(x, y)
+    f.suptitle('Roll (top) and Pitch (bottom) in radians')
 
+    # roll comparison
     y = df_1['roll'].to_list()
     var_1 = np.var(y)
     x = np.linspace(0, n, n)
-    plt.plot(x, y[:n], label='roll raw: var ' + "{0:.5f}".format(var_1), color='black')
-    
+    axarr[0].plot(x, y[:n])
+
     y = df_2['roll'].to_list()
     var_2 = np.var(y)
     x = np.linspace(0, n, n)
-    plt.plot(x, y[:n], label='roll calibrated: var ' + "{0:.5f}".format(var_2), color='red')
-    
-    plt.ylim([-0.1, 0.1])
-    plt.legend()
-    plt.show()
+    axarr[0].plot(x, y[:n])
 
     if var_1 < var_2:
-        print("Variance of {} is smaller: {}".format(filename, var_1 * 180.0/PI))
+        print("Variance of {} is smaller: {}".format(filename, var_1 * 180.0/math.pi))
     else:
-        print("Variance of {} is smaller: {}".format(filename_2, var_2 * 180.0/PI))
-        
+        print("Variance of {} is smaller: {}".format(filename_2, var_2 * 180.0/math.pi))
 
+    # pitch comparison
     y = df_1['pitch'].to_list()
     var_1 = np.var(y)
     x = np.linspace(0, n, n)
-    plt.plot(x, y[:n], label='pitch raw: var ' + "{0:.5f}".format(var_1), color='black')
-    
+    axarr[1].plot(x, y[:n])
+
     y = df_2['pitch'].to_list()
     var_2 = np.var(y)
     x = np.linspace(0, n, n)
-    plt.plot(x, y[:n], label='pitch calibrated: var ' + "{0:.5f}".format(var_2), color='red')
-    
-    plt.ylim([-0.1, 0.1])
-    plt.legend()
-    plt.show()
+    axarr[1].plot(x, y[:n])
 
     if var_1 < var_2:
-        print("Variance of {} is smaller: {}".format(filename, var_1 * 180.0/PI))
+        print("Variance of {} is smaller: {}".format(filename, var_1 * 180.0/math.pi))
     else:
-        print("Variance of {} is smaller: {}".format(filename_2, var_2 * 180.0/PI))
-        
+        print("Variance of {} is smaller: {}".format(filename_2, var_2 * 180.0/math.pi))
+
+    plt.legend()
+    plt.show()
 
 file_1 = sys.argv[1]
 file_1 = sys.argv[1]
 file_2 = sys.argv[2]
 n = int(sys.argv[3])
-plot(file_1, file_2, n) 
+plot(file_1, file_2, n)
 
 
