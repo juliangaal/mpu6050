f0d0dd3bc907e83166e6e49f079036a5b0079f41
Julian Gaal <gjulian@uos.de>
2019-04-25 12:39:22 +0200

    clean labels

--- viz/plot.py
+++ viz/plot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+import math
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -10,44 +11,49 @@ def plot(filename: str, filename_2: str, num_data_points: int):
     df_2 = pd.read_csv(filename_2, delimiter = ',', names=['roll', 'pitch'])
     n = num_data_points
 
+    f, axarr = plt.subplots(2, sharex=True)
+    f.suptitle('Roll (top) and Pitch (bottom)')
+    axarr[0].plot(x, y)
+    axarr[1].scatter(x, y)
+
     y = df_1['roll'].to_list()
-    x = np.linspace(0, n, n)
-    plt.plot(x, y[:n], label='roll raw: var ' + str(np.var(y)), color='black')
     var_1 = np.var(y)
+    x = np.linspace(0, n, n)
+    plt.plot(x, y[:n], label='roll raw: var ' + "{0:.5f}".format(var_1), color='black')
     
     y = df_2['roll'].to_list()
-    x = np.linspace(0, n, n)
-    plt.plot(x, y[:n], label='roll calibrated: var ' + str(np.var(y)), color='red')
     var_2 = np.var(y)
+    x = np.linspace(0, n, n)
+    plt.plot(x, y[:n], label='roll calibrated: var ' + "{0:.5f}".format(var_2), color='red')
     
     plt.ylim([-0.1, 0.1])
     plt.legend()
     plt.show()
 
     if var_1 < var_2:
-        print("Variance of {} is smaller: {}".format(filename, var_1))
+        print("Variance of {} is smaller: {}".format(filename, var_1 * 180.0/PI))
     else:
-        print("Variance of {} is smaller: {}".format(filename_2, var_2))
+        print("Variance of {} is smaller: {}".format(filename_2, var_2 * 180.0/PI))
         
 
     y = df_1['pitch'].to_list()
-    x = np.linspace(0, n, n)
-    plt.plot(x, y[:n], label='pitch raw: var ' + str(np.var(y)), color='black')
     var_1 = np.var(y)
+    x = np.linspace(0, n, n)
+    plt.plot(x, y[:n], label='pitch raw: var ' + "{0:.5f}".format(var_1), color='black')
     
     y = df_2['pitch'].to_list()
-    x = np.linspace(0, n, n)
-    plt.plot(x, y[:n], label='pitch calibrated: var ' + str(np.var(y)), color='red')
     var_2 = np.var(y)
+    x = np.linspace(0, n, n)
+    plt.plot(x, y[:n], label='pitch calibrated: var ' + "{0:.5f}".format(var_2), color='red')
     
     plt.ylim([-0.1, 0.1])
     plt.legend()
     plt.show()
 
     if var_1 < var_2:
-        print("Variance of {} is smaller: {}".format(filename, var_1))
+        print("Variance of {} is smaller: {}".format(filename, var_1 * 180.0/PI))
     else:
-        print("Variance of {} is smaller: {}".format(filename_2, var_2))
+        print("Variance of {} is smaller: {}".format(filename_2, var_2 * 180.0/PI))
         
 
 file_1 = sys.argv[1]
--- viz/plot_rot.py
+++ viz/plot_rot.py
@@ -11,14 +11,14 @@ def plot(filename: str, filename_2: str, num_data_points: int):
     n = num_data_points
 
     y = df_1['x'].to_list()
-    x = np.linspace(0, n, n)
-    plt.plot(x, y[:n], label='x raw: var ' + str(np.var(y)), color='black')
     var_1 = np.var(y)
+    x = np.linspace(0, n, n)
+    plt.plot(x, y[:n], label='x raw: var ' + "{0:.5f}".format(var_1), color='black')
     
     y = df_2['x'].to_list()
-    x = np.linspace(0, n, n)
-    plt.plot(x, y[:n], label='x calib: var ' + str(np.var(y)), color='red')
     var_2 = np.var(y)
+    x = np.linspace(0, n, n)
+    plt.plot(x, y[:n], label='x calib: var ' + "{0:.5f}".format(var_2), color='red')
     
     plt.ylim([-0.1, 0.1])
     plt.legend()
@@ -31,14 +31,14 @@ def plot(filename: str, filename_2: str, num_data_points: int):
         
 
     y = df_1['y'].to_list()
-    x = np.linspace(0, n, n)
-    plt.plot(x, y[:n], label='y raw: var ' + str(np.var(y)), color='black')
     var_1 = np.var(y)
+    x = np.linspace(0, n, n)
+    plt.plot(x, y[:n], label='y raw: var ' + "{0:.5f}".format(var_1), color='black')
     
     y = df_2['y'].to_list()
-    x = np.linspace(0, n, n)
-    plt.plot(x, y[:n], label='y calib: var ' + str(np.var(y)), color='red')
     var_2 = np.var(y)
+    x = np.linspace(0, n, n)
+    plt.plot(x, y[:n], label='y calib: var ' + "{0:.5f}".format(var_2), color='red')
     
     plt.ylim([-0.1, 0.1])
     plt.legend()
@@ -51,14 +51,14 @@ def plot(filename: str, filename_2: str, num_data_points: int):
         
 
     y = df_1['z'].to_list()
-    x = np.linspace(0, n, n)
-    plt.plot(x, y[:n], label='z raw: var ' + str(np.var(y)), color='black')
     var_1 = np.var(y)
+    x = np.linspace(0, n, n)
+    plt.plot(x, y[:n], label='z raw: var ' + "{0:.5f}".format(var_1), color='black')
     
     y = df_2['z'].to_list()
-    x = np.linspace(0, n, n)
-    plt.plot(x, y[:n], label='z calib: var ' + str(np.var(y)), color='red')
     var_2 = np.var(y)
+    x = np.linspace(0, n, n)
+    plt.plot(x, y[:n], label='z calib: var ' + "{0:.5f}".format(var_2), color='red')
     
     plt.ylim([-0.1, 0.1])
     plt.legend()
