b9dbcf889612f8f566cfe8376fc86f3eac31d0ef
Julian Gaal <gjulian@uos.de>
2019-05-08 19:02:28 +0200

    remove unnecessary spaces

--- src/registers.rs
+++ src/registers.rs
@@ -7,32 +7,26 @@ pub enum Registers {
     SLAVE_ADDR = 0x68,
     /// Internal register to check slave addr
     WHOAMI = 0x75,
-
     /// High Bytle Register Gyro x orientation
     GYRO_REGX_H = 0x43,
     /// High Bytle Register Gyro y orientation
     GYRO_REGY_H = 0x45,
     /// High Bytle Register Gyro z orientation
     GYRO_REGZ_H = 0x47,
- 
     /// High Byte Register Calc roll
     ACC_REGX_H = 0x3b,
     /// High Byte Register Calc pitch
     ACC_REGY_H = 0x3d,
     /// High Byte Register Calc yaw
     ACC_REGZ_H = 0x3f,
-
     /// High Byte Register Temperature
     TEMP_OUT_H = 0x41,
-
     /// Register to control chip waking from sleep, enabling sensors, default: sleep
     POWER_MGMT_1 = 0x6b, 
     /// Internal clock
     POWER_MGMT_2 = 0x6c, 
-
     /// Accelerometer config register
     ACCEL_CONFIG  = 0x1c,
-
     /// gyro config register
     GYRO_CONFIG = 0x1b,
 }
